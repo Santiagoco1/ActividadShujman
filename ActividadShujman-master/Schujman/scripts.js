3323c8babc3322b43f0d85d1e264d988111ced8e
Santiago Có <51292523+Santiagoco1@users.noreply.github.com>
2020-04-25 13:30:50 -0300

    Add files via upload

--- ActividadShujman-master/Schujman/scripts.js
+++ ActividadShujman-master/Schujman/scripts.js
@@ -1,8 +1,13 @@
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
-    }
-  }
+function show_modal(id) {
+    document.getElementById(id).style.display='block';
+
+    document.body.style.overflow = "hidden";
+    document.body.style.height = "100vh";
+}
+
+function hide_modal(id) {
+    document.getElementById(id).style.display='none';
+    
+    document.body.style.overflow = "scroll";
+    document.body.style.height = "100vh";
+}
